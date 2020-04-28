a9c13b85b8b99f622be1eafa35f5bae74ac6f5f7
Traker01 <kamil1e7@interia.pl>
2020-04-29 00:13:14 +0200

    cwiczenie

--- Form1.cs
+++ Form1.cs
@@ -20,7 +20,7 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
